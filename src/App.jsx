51e262d0bf19ef2876f5c1a496924b37a6989562
Monde Sineke <nsineke@gmail.com>
2023-09-04 20:12:09 +0200

    ✨ feat(app*): make navbar
    Create dynamic navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import { Outlet, Link } from "@tanstack/react-router";
+import { Outlet } from "@tanstack/react-router";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <>
       <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/Login">Login</Link>
-            </li>
-          </ul>
-        </nav>
+        <Navbar />
       </header>
 
       <main>
